4b29e6bd52bb6c6cfb2ece18f502ff6da81a072f
Balazs Kocso <kocsob@users.noreply.github.com>
2017-12-26 23:41:06 +0100

    setup.py: Add python packaging to the project

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+from syncipy.version import __version__
+
+setup(
+    name='syncipy',
+    version=__version__,
+    description='Python package to synchronize IP address with dynamic DNS services',
+    author='Balazs Kocso',
+    author_email='kocsob@users.noreply.github.com',
+    packages=['syncipy'],
+    install_requires=[
+        'requests',
+    ],
+    extras_require={
+        'dev': [
+            'flake8',
+            'pytest',
+            'pytest-pep8',
+            'pytest-cov',
+        ]
+    },
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet :: Name Service (DNS)",
+    ],
+)
